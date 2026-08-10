9a09dea4a603ee4ebe9316bc4f33aaaa1dc0eeb3
pouletaucrabe <emytheplatypus@gmail.com>
2026-08-10 20:31:46 +0200

    Connect tier list to Google Sheets

--- qr/config.js
+++ qr/config.js
@@ -1,1 +1,1 @@
-window.MAELSTROM_RESULTS_ENDPOINT = "";
+window.MAELSTROM_RESULTS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxJJZRep9vVy2d0ZHMIRRIrMe4lbzRNNkL8iQZ2wrwGuGt2aqob6h7sb1WsGdcSRHKuyQ/exec";
