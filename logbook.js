6113f9979a805a48850817899fb66ad78e86be6b
pouletaucrabe <emytheplatypus@gmail.com>
2026-08-22 11:25:53 +0200

    Simplify logbook page changes

--- logbook.js
+++ logbook.js
@@ -1,32 +1,32 @@
 const logbookButton = document.querySelector("[data-logbook-next]");
-const logbookTurnPage = document.querySelector("[data-logbook-turn-page]");
 const logbookStatus = document.querySelector("[data-logbook-status]");
+const logbookSheet = document.querySelector("[data-logbook-sheet]");
 const LOGBOOK_PAGE_COUNT = 6;
+const LOGBOOK_PAGE_POSITIONS = [
+  ["50%", "50%"],
+  ["48%", "47%"],
+  ["52%", "51%"],
+  ["46%", "54%"],
+  ["54%", "49%"],
+  ["50%", "56%"],
+];
 let logbookPage = 1;
 
 function updateLogbookStatus() {
   if (logbookStatus) {
     logbookStatus.textContent = `Page ${logbookPage}`;
   }
+
+  if (logbookSheet) {
+    const [x, y] = LOGBOOK_PAGE_POSITIONS[logbookPage - 1] || LOGBOOK_PAGE_POSITIONS[0];
+    logbookSheet.style.setProperty("--logbook-page-x", x);
+    logbookSheet.style.setProperty("--logbook-page-y", y);
+  }
 }
 
 logbookButton?.addEventListener("click", () => {
-  if (!logbookTurnPage || logbookTurnPage.classList.contains("is-turning")) {
-    return;
-  }
-
-  logbookTurnPage.classList.add("is-turning");
-  logbookButton.disabled = true;
-
-  window.setTimeout(() => {
-    logbookPage = logbookPage >= LOGBOOK_PAGE_COUNT ? 1 : logbookPage + 1;
-    updateLogbookStatus();
-  }, 360);
-
-  window.setTimeout(() => {
-    logbookTurnPage.classList.remove("is-turning");
-    logbookButton.disabled = false;
-  }, 760);
+  logbookPage = logbookPage >= LOGBOOK_PAGE_COUNT ? 1 : logbookPage + 1;
+  updateLogbookStatus();
 });
 
 updateLogbookStatus();
