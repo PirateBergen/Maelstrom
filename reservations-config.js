6bca73099b1e243f51d12558d6be9eaa8d48e88f
pouletaucrabe <emytheplatypus@gmail.com>
2026-08-27 12:27:56 +0200

    Connect live reservation endpoint

--- reservations-config.js
+++ reservations-config.js
@@ -1,1 +1,1 @@
-window.MAELSTROM_RESERVATION_ENDPOINT = "https://script.google.com/macros/s/AKfycbx-kQvgktCX3LGadWJGOOEilIgtuMGPqCdhIIpo07JM-pBT8F5lM7ZX4stTntZ6kN67/exec";
+window.MAELSTROM_RESERVATION_ENDPOINT = "https://script.google.com/macros/s/AKfycbx6zRUgPQ2_aLBGFqaD-RxX1hLBnp8s7XqyfLeOLXLw6p4srzIbXcPeAS-86R_eOSfo/exec";
