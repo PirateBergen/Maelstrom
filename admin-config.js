2c1d2a15dc8740ebfbe8c5267b7f5b4d1921074e
pouletaucrabe <emytheplatypus@gmail.com>
2026-09-03 12:08:09 +0200

    Connect administration entry to private Apps Script deployment

--- admin-config.js
+++ admin-config.js
@@ -1,3 +1,3 @@
 // Only the URL of the separate PRIVATE Apps Script deployment goes here.
 // Never put Google credentials, an administrator password or Cloudinary secrets in this file.
-window.MAELSTROM_ADMIN_URL = "";
+window.MAELSTROM_ADMIN_URL = "https://script.google.com/macros/s/AKfycbwFofBE5stmKV-HRTDcRgBpUk6apc-kJ7fhWHqQclJWbreXpYhlYG6Fi09G3nDHYzEB/exec";
